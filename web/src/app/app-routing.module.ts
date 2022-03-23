a95d9d6ab32dc10a308b39dd0576e8aeb26b97ba
William Teixeira <williderteixeira@gmail.com>
2022-03-23 00:20:34 -0400

    feat: create the initial dockerfiles

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ProjectListComponent } from './project-list/project-list.component';
+import { AppComponent } from './app.component';
+import { ProjectFormComponent } from './project-form/project-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-project', component: ProjectFormComponent },
+  { path: '', component: ProjectListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
