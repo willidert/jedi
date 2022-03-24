a129c0bf9ea81b8847c33284cec7735933f4db6b
William Teixeira <williderteixeira@gmail.com>
2022-03-23 20:55:00 -0400

    fix: update the API_URL in service

--- web/src/app/project.service.ts
+++ web/src/app/project.service.ts
@@ -1,5 +1,5 @@
 import { IProject } from './IProject';
-import { API_PATH } from './../environments/environment';
+// import { API_PATH } from './../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, take } from 'rxjs';
@@ -8,26 +8,33 @@ import { Observable, take } from 'rxjs';
   providedIn: 'root',
 })
 export class ProjectService {
-  constructor(private client: HttpClient) {}
+  readonly API_URL: string;
+
+  constructor(private client: HttpClient) {
+    this.API_URL = 'http://localhost:5000';
+  }
 
   get_projects(): Observable<IProject[]> {
-    return this.client.get<IProject[]>(`${API_PATH}/projects`);
+    return this.client.get<IProject[]>(`${this.API_URL}/projects`);
   }
 
   get_project_by_id(id: string): Observable<IProject> {
-    return this.client.get<IProject>(`${API_PATH}/projects/${id}`);
+    return this.client.get<IProject>(`${this.API_URL}/projects/${id}`);
   }
 
   create_project(project: IProject): Observable<IProject> {
-    return this.client.post<IProject>(`${API_PATH}/projects`, project);
+    return this.client.post<IProject>(`${this.API_URL}/projects`, project);
   }
 
   delete_project(id: number) {
-    return this.client.delete(`${API_PATH}/projects/${id}`).pipe(take(1));
+    return this.client.delete(`${this.API_URL}/projects/${id}`).pipe(take(1));
   }
 
   update_project(project: IProject, id: number): Observable<IProject> {
-    return this.client.patch<IProject>(`${API_PATH}/projects/${id}`, project);
+    return this.client.patch<IProject>(
+      `${this.API_URL}/projects/${id}`,
+      project
+    );
   }
 
   save(project: IProject): Observable<IProject> {
