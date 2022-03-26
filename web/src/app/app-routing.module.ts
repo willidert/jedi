73c001623bd541a8fc6433950717ab326f80f49b
William Teixeira <williderteixeira@gmail.com>
2022-03-26 14:44:38 -0400

    feat: add routing for projects

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { CalcComponent } from './calc/calc.component';
-import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectFormComponent } from './project-form/project-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'create-project', component: ProjectFormComponent },
-  { path: '', component: ProjectListComponent },
+  { path: '', redirectTo: 'projects', pathMatch: 'full' },
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./projects/projects.module').then((m) => m.ProjectsModule),
+  },
   { path: 'calc/:id', component: CalcComponent },
   { path: 'edit/:id', component: ProjectFormComponent },
 ];
