ccb7b148fac493a53bda2927f2777726937ac2e7
William Teixeira <williderteixeira@gmail.com>
2022-03-26 23:45:16 -0400

    fix: fixing api bug in database connection

--- api/database.py
+++ api/database.py
@@ -4,17 +4,15 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
+# try:
+#     from dotenv import load_dotenv
+#     load_dotenv()
 
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-except AttributeError:
-    DATABASE_URL = "sqlite:///./sql_app.db"
-    engine = create_engine(DATABASE_URL)
+#     DATABASE_URL = os.getenv('DATABASE_URL')
+#     engine = create_engine(DATABASE_URL)
+# except Exception:
+DATABASE_URL = "sqlite:///./sql_app.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
