d24071dc60e2962db6232ae1954c01ff69cf5d8e
William Teixeira <williderteixeira@gmail.com>
2022-03-27 18:38:45 -0400

    feat: delete project

--- web/src/app/projects/projects/projects.component.ts
+++ web/src/app/projects/projects/projects.component.ts
@@ -1,9 +1,10 @@
-import { ProjectService } from './../services/projects.service';
-import { catchError, Observable, of } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { IProject } from 'src/app/projects/projects/model/IProject';
 import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { IProject } from 'src/app/projects/projects/model/IProject';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+import { ProjectService } from './../services/projects.service';
 
 @Component({
   selector: 'app-projects',
@@ -12,7 +13,14 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 })
 export class ProjectsComponent implements OnInit {
   projects$: Observable<IProject[]>;
-  displayedColumns = ['risk', 'name', 'begin_date', 'end_date', 'value'];
+  displayedColumns = [
+    'risk',
+    'name',
+    'begin_date',
+    'end_date',
+    'value',
+    'actions',
+  ];
 
   constructor(private service: ProjectService, public dialog: MatDialog) {
     this.projects$ = this.service.get_projects().pipe(
@@ -28,6 +36,12 @@ export class ProjectsComponent implements OnInit {
   onError(errorMsg: string) {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMsg,
+    });
+  }
+
+  onDelete(id: number): void {
+    this.service.delete_project(id).subscribe((success) => {
+      this.projects$ = this.service.get_projects();
     });
   }
 }
