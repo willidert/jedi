d4e6470d4d0db7ae13eb02b7cac4e6858db751fb
William Teixeira <williderteixeira@gmail.com>
2022-03-26 23:20:58 -0400

    feat: add error dialog

--- web/src/app/projects/projects.module.ts
+++ web/src/app/projects/projects.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects/projects.component';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ProjectsComponent],
-  imports: [CommonModule, ProjectsRoutingModule, AppMaterialModule],
+  imports: [
+    CommonModule,
+    ProjectsRoutingModule,
+    AppMaterialModule,
+    SharedModule,
+  ],
 })
 export class ProjectsModule {}
--- web/src/app/projects/projects/projects.component.ts
+++ web/src/app/projects/projects/projects.component.ts
@@ -2,6 +2,8 @@ import { catchError, Observable, of } from 'rxjs';
 import { ProjectService } from './../../project.service';
 import { Component, OnInit } from '@angular/core';
 import { IProject } from 'src/app/projects/projects/model/IProject';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-projects',
@@ -12,13 +14,20 @@ export class ProjectsComponent implements OnInit {
   projects$: Observable<IProject[]>;
   displayedColumns = ['risk', 'name', 'begin_date', 'end_date', 'value'];
 
-  constructor(private service: ProjectService) {
+  constructor(private service: ProjectService, public dialog: MatDialog) {
     this.projects$ = this.service.get_projects().pipe(
       catchError((error) => {
+        this.onError('Loading projects failed!');
         return of([]);
       })
     );
   }
 
   ngOnInit(): void {}
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
 }
