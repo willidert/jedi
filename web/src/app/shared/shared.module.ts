9860424d9cc4681093bd93900de36a99ce6a023a
William Teixeira <williderteixeira@gmail.com>
2022-04-29 09:10:07 -0400

    chore: remove unnecessary imports

--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { RiskPipe } from './pipes/risk.pipe';
-import { MAT_DATE_FORMATS } from '@angular/material/core';
-import { MY_DATE } from './util/MY_DATE';
 import { CalcDialogComponent } from './calc-dialog/calc-dialog.component';
 
 @NgModule({
   declarations: [ErrorDialogComponent, RiskPipe, CalcDialogComponent],
   imports: [CommonModule, AppMaterialModule],
-  // providers: [{ provide: MAT_DATE_FORMATS, useValue: MY_DATE }],
   exports: [ErrorDialogComponent, RiskPipe],
 })
 export class SharedModule {}
